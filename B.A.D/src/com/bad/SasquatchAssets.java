ab118a2d63b0e43e54f761afa496d266a9866b54
unknown <kyle.t.doyle@AFS72WSGTDVWJ7.us.accenturefederal.com>
2013-08-24 22:22:06 -0400

    Canada commit

--- B.A.D/src/com/bad/SasquatchAssets.java
+++ B.A.D/src/com/bad/SasquatchAssets.java
@@ -23,9 +23,11 @@ public class SasquatchAssets {
 	public static float SASQ_LOW_PUNCH = 0.16f;
 	public static float SASQ_STAND = 0.33f;
 	public static float SASQ_WALK = 0.16f;
+	public static float SASQ_TUMBLE = 0.33f;
 	public static Array<TextureRegion> punchAnim;
 	public static Array<TextureRegion> standAnim;
 	public static Array<TextureRegion> walkingAnim;
+	public static Array<TextureRegion> tumbleAnim;
 	public static Texture background;
 	public static Texture splash;
 	
@@ -33,6 +35,8 @@ public class SasquatchAssets {
 		Texture sasquatchSMap = new Texture(Gdx.files.internal("assets/Sasquatch_clear.png"));
 		sasquatchAtlas = new TextureAtlas(Gdx.files.internal("assets/Sasquatch.atlas"));
 		background = new Texture(Gdx.files.internal("assets/moon_background.png"));
+		
+		int textureOffset = 10;
 		
 		stand = sasquatchAtlas.findRegion("stand1");
 		stand2 = sasquatchAtlas.findRegion("stand2");
@@ -55,10 +59,34 @@ public class SasquatchAssets {
 //		standAnim.add(stand4);
 		
 		walkingAnim = new Array<TextureRegion>();
+		tumbleAnim = new Array<TextureRegion>();
 		
-		for(int i=0; i < 12; i++){
-			walkingAnim.add(new TextureRegion(sasquatchSMap, 10+(i*136), 136, 136, 100));
+		int positionX;
+		int sizeOffset = 0;
+		for(int i=0; i < 12; i++){ 	
+			textureOffset = 10;
+			if(i == 5){
+				textureOffset += 12;
+			} else if (i == 3 || i == 7){
+				textureOffset += 5;
+			}
+			positionX =  textureOffset+(i*136);
+			walkingAnim.add(new TextureRegion(sasquatchSMap, positionX, 136, 136, 100));
+			int rightX = positionX+136+sizeOffset;
+			Gdx.app.debug("position check - tumble","i: " + i + " Left X: " + positionX + " Right X: " + rightX);
 		}
+		
+		textureOffset = 1655;
+		for(int i=0; i < 4; i++){
+			if(i > 1){
+				sizeOffset += 10;
+			} else if(i == 1){
+				sizeOffset += 7;
+			}
+			positionX = textureOffset+(i*115) + sizeOffset;
+			tumbleAnim.add(new TextureRegion(sasquatchSMap, positionX, 155, 115+sizeOffset, 90));
+		}
+		
 	}
 	
 	
--- B.A.D/src/com/bad/TestScreen.java
+++ B.A.D/src/com/bad/TestScreen.java
@@ -25,8 +25,13 @@ public class TestScreen extends GameScreen {
 	Animation stand;
 	Animation punch;
 	Animation walking;
+	Animation walkingBackwards;
+	Animation tumble;
 	
 	int sasquatchScale;
+	int sasquatchPosX;
+	int sasquatchPosY;
+	int sasquatchVelocity;
 	
 	float stateTime;
 	float punchTime;
@@ -34,6 +39,9 @@ public class TestScreen extends GameScreen {
 	
 	public TestScreen(Game game) {
 		super(game);
+		sasquatchPosX = 0;
+		sasquatchPosY = 0;
+		sasquatchVelocity = 300;
 	}
 
 	@Override
@@ -55,23 +63,47 @@ public class TestScreen extends GameScreen {
 		} else {
 			if(Gdx.input.isKeyPressed(Keys.ESCAPE)){
 				System.exit(0);
-			} else if(Gdx.input.isKeyPressed(Keys.RIGHT)){
+			} else if(Gdx.input.isKeyPressed(Keys.LEFT)){
 				currentFrame = walking.getKeyFrame(stateTime, true);
-			}
-			else if(Gdx.input.isKeyPressed(Keys.SPACE)) {
+				sasquatchPosX = (int) (sasquatchPosX-(sasquatchVelocity*Gdx.graphics.getDeltaTime()));
+			} else if(Gdx.input.isKeyPressed(Keys.UP)){
+				currentFrame = tumble.getKeyFrame(stateTime, true);
+				sasquatchPosX = (int) (sasquatchPosX+(sasquatchVelocity*Gdx.graphics.getDeltaTime()));
+				sasquatchPosY = (int) (sasquatchPosY+(sasquatchVelocity*Gdx.graphics.getDeltaTime()));
+			} else if(Gdx.input.isKeyPressed(Keys.RIGHT)){
+				//have to animate similar to punch
+				currentFrame = walking.getKeyFrame(stateTime, true);
+				sasquatchPosX = (int) (sasquatchPosX+(sasquatchVelocity*Gdx.graphics.getDeltaTime()));
+			} else if(Gdx.input.isKeyPressed(Keys.SPACE)) {
 				stateTime = 0.0f;
 				currentFrame = punch.getKeyFrame(stateTime);
 				punchTime = (SasquatchAssets.SASQ_LOW_PUNCH * SasquatchAssets.punchAnim.size) + stateTime;
 				Gdx.app.debug("Random","Key Press - Punch Frame: " + punch.getKeyFrameIndex(stateTime) + " StateTime: " + stateTime + "\nPunch Time: " + punchTime + "\n\n");
 				actionAnimation = true;
-			}
-			else {
+			} else if (Gdx.input.isKeyPressed(Keys.ENTER)){
+
+			} else {
 				currentFrame = stand.getKeyFrame(stateTime, true);
 			}
 		}
 		
 		batch.begin();
 		batch.draw(background, 0, 0, RandomFighterGame.width, RandomFighterGame.height);
+		batch.draw(currentFrame, sasquatchPosX, sasquatchPosY, 0, 0, currentFrame.getRegionWidth(), currentFrame.getRegionHeight(), this.sasquatchScale, this.sasquatchScale, 0);
+//		Gdx.app.debug("positions", "X: " + sasquatchPosX + " Y: " + sasquatchPosY);
+//		Gdx.app.debug("positions", "width: " + currentFrame.getRegionWidth() + " height: " +  currentFrame.getRegionHeight());
+//		sasquatchPosX = 0;
+//		sasquatchPosY = 0;
+//		for(int i = 0; i < SasquatchAssets.tumbleAnim.size; i++){
+//			batch.draw(SasquatchAssets.tumbleAnim.get(i), sasquatchPosX, sasquatchPosY, 0, 0, SasquatchAssets.tumbleAnim.get(i).getRegionWidth(), SasquatchAssets.tumbleAnim.get(i).getRegionHeight(), this.sasquatchScale, this.sasquatchScale, 0);
+////			sasquatchPosX += SasquatchAssets.tumbleAnim.get(i).getRegionWidth();
+//			sasquatchPosY += SasquatchAssets.tumbleAnim.get(i).getRegionHeight() + 10;
+//			if(sasquatchPosY > RandomFighterGame.height){
+//				sasquatchPosX = SasquatchAssets.tumbleAnim.get(i).getRegionHeight();
+//				sasquatchPosY = 0;
+//			}
+//			Gdx.app.debug("positions", "X: " + sasquatchPosX + " Y: " + sasquatchPosY);
+//		}
 		batch.end();
 	}
 
@@ -79,11 +111,13 @@ public class TestScreen extends GameScreen {
 	public void show() {
 		background = new Texture(Gdx.files.internal("assets/moon_background.png"));
 		batch = new SpriteBatch();
-		batch.getProjectionMatrix().setToOrtho2D(0, 0, RandomFighterGame.width, RandomFighterGame.height);
+//		batch.getProjectionMatrix().setToOrtho2D(0, 0, RandomFighterGame.width, RandomFighterGame.height);
 		
 		punch = new Animation(SasquatchAssets.SASQ_LOW_PUNCH, SasquatchAssets.punchAnim);
 		stand = new Animation(SasquatchAssets.SASQ_STAND, SasquatchAssets.standAnim, Animation.LOOP);
 		walking = new Animation(SasquatchAssets.SASQ_WALK, SasquatchAssets.walkingAnim, Animation.LOOP);
+		walkingBackwards = new Animation(SasquatchAssets.SASQ_WALK, SasquatchAssets.walkingAnim, Animation.LOOP_REVERSED);
+		tumble = new Animation(SasquatchAssets.SASQ_TUMBLE, SasquatchAssets.tumbleAnim, Animation.LOOP);
 		sasquatchScale = 2;
 	}
 
